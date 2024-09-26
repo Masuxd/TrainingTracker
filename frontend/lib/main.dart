3e0f6e2992b093135e794a8126126b1d149c61a7
mkytomies <moonakytomies@gmail.com>
2024-09-26 14:48:00 +0300

    Started on the login/signup screen

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,67 +1,17 @@
 import 'package:flutter/material.dart';
-import 'api_service.dart';
+import './features/auth/login_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Main());
 }
 
-class MyApp extends StatelessWidget {
+class Main extends StatelessWidget {
+  const Main({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final ApiService apiService = ApiService();
-  Future<List<dynamic>>? examples;
-
-  @override
-  void initState() {
-    super.initState();
-    examples = apiService.fetchExamples();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Training Tracker'),
-      ),
-      body: FutureBuilder<List<dynamic>>(
-        future: examples,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(child: Text('No data found'));
-          } else {
-            return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                final item = snapshot.data![index];
-                return ListTile(
-                  title: Text(item['name']),
-                  subtitle: Text(item['description']),
-                );
-              },
-            );
-          }
-        },
-      ),
+      home: LoginScreen(),
     );
   }
 }
