49b464bab927ef144b11bc88ee551b82df46db66
sini.makela <sini.m.makela@tuni.fi>
2024-10-17 18:24:54 +0300

    Kalenteri tallentaa nyt myös saman päivän tapahtuman

--- frontend/lib/features/calendar/calendar.dart
+++ frontend/lib/features/calendar/calendar.dart
@@ -39,8 +39,10 @@ class _MyAppState extends State<MyApp> {
   void _onDaySelected(DateTime day, DateTime focusedDay) {
     setState(() {
       today = day;
+      todayEvents.value = _getEventsForDay(day);
+
     });
-    todayEvents.value = _getEventsForDay(day);
+
   }
 
   @override
@@ -51,91 +53,90 @@ class _MyAppState extends State<MyApp> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(),
-      home: Scaffold(
-        appBar: AppBar(title: const Text("Training Calendar")), // Otsikko
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(
-                  scrollable: true,
-                  title: const Text("Event name"),
-                  content: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: TextField(
-                      controller: _eventController,
-                    ),
+Widget build(BuildContext context) {
+  return MaterialApp(
+    theme: ThemeData(),
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Training Calendar"),
+      ), // Otsikko
+      floatingActionButton: FloatingActionButton(
+        //nappi jolla voi lisätä tapahtuman
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                scrollable: true,
+                title: Text("Event name"),
+                content: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: TextField(
+                    controller: _eventController,
                   ),
-                  actions: [
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          if (events[today] != null) {
-                            events[today]!.add(Event(_eventController.text));
-                          } else {
-                            events[today] = [Event(_eventController.text)];
-                          }
-                          todayEvents.value = _getEventsForDay(today);
-                        });
-                        Navigator.of(context).pop();
-                      },
-                      child: const Text("Submit"),
-                    )
-                  ],
+                ),
+                actions: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // lisää tapahtuma
+                      events.addAll({
+                        today: [Event(_eventController.text)]
+                      });
+                      Navigator.of(context).pop();
+                      todayEvents.value = _getEventsForDay(today!);
+                    },
+                    child: Text("Submit"),
+                  )
+                ],
+              );
+            },
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+      body: Column(
+        children: [
+          TableCalendar(
+            headerStyle: const HeaderStyle(
+              formatButtonVisible: false,
+              titleCentered: true,
+            ),
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            calendarFormat: CalendarFormat.month,
+            selectedDayPredicate: (day) => isSameDay(day, today),
+            firstDay: DateTime.utc(2010, 1, 1),
+            lastDay: DateTime.utc(2040, 12, 31),
+            focusedDay: today,
+            onDaySelected: _onDaySelected,
+            eventLoader: _getEventsForDay,
+          ),
+          SizedBox(height: 8.0),
+          Expanded(
+            child: ValueListenableBuilder<List<Event>>(
+              valueListenable: todayEvents,
+              builder: (context, value, _) {
+                return ListView.builder(
+                  itemCount: value.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: ListTile(
+                        onTap: () => print(""),
+                        title: Text('${value[index]}'),
+                      ),
+                    );
+                  },
                 );
               },
-            );
-          },
-          child: const Icon(Icons.add),
-        ),
-        body: Column(
-          children: [
-            TableCalendar(
-              headerStyle: const HeaderStyle(
-                formatButtonVisible: false,
-                titleCentered: true,
-              ),
-              startingDayOfWeek: StartingDayOfWeek.monday,
-              calendarFormat: CalendarFormat.month,
-              selectedDayPredicate: (day) => isSameDay(day, today),
-              firstDay: DateTime.utc(2010, 1, 1),
-              lastDay: DateTime.utc(2040, 12, 31),
-              focusedDay: today,
-              onDaySelected: _onDaySelected,
-              eventLoader: _getEventsForDay,
             ),
-            const SizedBox(height: 8.0),
-            Expanded(
-              child: ValueListenableBuilder<List<Event>>(
-                valueListenable: todayEvents,
-                builder: (context, value, _) {
-                  return ListView.builder(
-                    itemCount: value.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        margin: const EdgeInsets.symmetric(
-                            horizontal: 12, vertical: 4),
-                        decoration: BoxDecoration(
-                          border: Border.all(),
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: ListTile(
-                          onTap: () => print("Event tapped: ${value[index]}"),
-                          title: Text(value[index].name),
-                        ),
-                      );
-                    },
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-    );
-  }
+    ),
+  );
+}
 }
