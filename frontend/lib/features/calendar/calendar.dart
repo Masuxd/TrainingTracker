516e3edb35d01ae138a6dfe6f095ff41216a47b4
sini.makela <sini.m.makela@tuni.fi>
2024-10-17 13:30:36 +0300

    Lisäsin nappulafunktion jolla aukeaa tapahtuma ikkuna

--- frontend/lib/features/calendar/calendar.dart
+++ frontend/lib/features/calendar/calendar.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart'; // flutter calendar package
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(home: MyApp())); // korjaa materiallocalization errorin
 }
 
 class MyApp extends StatefulWidget {
@@ -12,9 +12,15 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
+class Event {
+  final String title;
+  Event(this.title);
+}
+
 class _MyAppState extends State<MyApp> {
   DateTime today = DateTime.now(); //päivämäärä
-  Map<DateTime, List> events = {};
+  Map<DateTime, List<Event>> events = {};
+  TextEditingController _eventController = TextEditingController();
   void _onDaySelected(DateTime day, DateTime focusedDay) {
     setState(() {
       today = day; // päivämäärä johon on klikattu
@@ -27,26 +33,42 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(),
       home: Scaffold(
         appBar: AppBar(title: const Text("Training Calendar")), // Otsikko
-        body: content(),
-      ),
-    );
-  }
-
-  Widget content() {
-    return Column(
-      children: [
-        const Text("Hellou"),
-        TableCalendar(
-          headerStyle: const HeaderStyle(
-              formatButtonVisible: false, titleCentered: true),
-          startingDayOfWeek: StartingDayOfWeek.monday,
-          selectedDayPredicate: (day) => isSameDay(day, today),
-          firstDay: DateTime.utc(2010, 1, 1),
-          lastDay: DateTime.utc(2040, 12, 31),
-          focusedDay: today,
-          onDaySelected: _onDaySelected,
+        floatingActionButton: FloatingActionButton(
+          //nappi jolla voi lisätä tapahtuman
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    scrollable: true,
+                    title: Text("Event name"),
+                    content: Padding(
+                      padding: EdgeInsets.all(8),
+                      child: TextField(
+                        controller: _eventController,
+                      ),
+                    ),
+                  );
+                });
+          },
+          child: Icon(Icons.add),
         ),
-      ],
+        body: Column(
+          children: [
+            TableCalendar(
+              headerStyle: const HeaderStyle(
+                  formatButtonVisible: false, titleCentered: true),
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              calendarFormat: CalendarFormat.month,
+              selectedDayPredicate: (day) => isSameDay(day, today),
+              firstDay: DateTime.utc(2010, 1, 1),
+              lastDay: DateTime.utc(2040, 12, 31),
+              focusedDay: today,
+              onDaySelected: _onDaySelected,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
