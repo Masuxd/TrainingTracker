b83e6d3e16b9214b7eb242eb20ce8cd8340bf439
mkytomies <moonakytomies@gmail.com>
2024-11-18 19:02:24 +0200

    Refactor: Rename id to sessionId and remove user model

--- frontend/lib/common/models/training_session.dart
+++ frontend/lib/common/models/training_session.dart
@@ -1,15 +1,15 @@
 import 'set.dart';
-import 'user.dart';
+//import 'user.dart';
 
 class TrainingSession {
-  final String id;
+  final String sessionId;
   final String userId;
   DateTime startTime;
   DateTime? endTime;
   List<Set> sets;
 
   TrainingSession({
-    required this.id,
+    required this.sessionId,
     required this.userId,
     required this.startTime,
     required this.endTime,
@@ -18,7 +18,7 @@ class TrainingSession {
 
   factory TrainingSession.fromJson(Map<String, dynamic> json) {
     return TrainingSession(
-      id: json['id'],
+      sessionId: json['id'],
       userId: json['userId'],
       startTime: DateTime.parse(json['startTime']),
       endTime: DateTime.parse(json['endTime']),
@@ -28,7 +28,7 @@ class TrainingSession {
 
   Map<String, dynamic> toJson() {
     return {
-      'id': id,
+      'id': sessionId,
       'userId': userId,
       'startTime': startTime.toIso8601String(),
       'endTime': endTime?.toIso8601String(),
