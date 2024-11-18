961188c43fc22632608dd65012a2695c64750291
mkytomies <moonakytomies@gmail.com>
2024-11-18 18:58:54 +0200

    Feat: Add widgetId to Set model

--- frontend/lib/common/models/set.dart
+++ frontend/lib/common/models/set.dart
@@ -2,29 +2,33 @@ import 'exercise.dart';
 import 'rep.dart';
 
 class Set {
-  final String id;
-  Exercise exercise;
+  final String setId;
+  final Exercise exercise;
   List<Rep> rep;
+  final String widgetId;
 
   Set({
-    required this.id,
+    required this.setId,
     required this.exercise,
     required this.rep,
+    required this.widgetId,
   });
 
   factory Set.fromJson(Map<String, dynamic> json) {
     return Set(
-      id: json['id'],
+      setId: json['id'],
       exercise: Exercise.fromJson(json['exercise']),
       rep: (json['rep'] as List).map((rep) => Rep.fromJson(rep)).toList(),
+      widgetId: json['widgetId'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'id': id,
+      'id': setId,
       'exercise': exercise.toJson(),
       'rep': rep.map((rep) => rep.toJson()).toList(),
+      'widgetId': widgetId,
     };
   }
 }
